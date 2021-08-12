03033a299db0c8e9dfc491c0e8099f497aa50fa6
vaibhav <vaibhav@kali.kali>
2021-08-12 13:34:40 +0530

    final

---
+++ src/main/java/com/practice/controller/MainController.java
@@ -0,0 +1,43 @@
+package com.practice.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/about")
+	public String about(Model model)
+	{
+		model.addAttribute("name","Vaibhav aagarwal");
+		List<String>list=new ArrayList<>();
+		list.add("Vaibhav");
+		list.add("Yash");
+		list.add("Harshit");
+		list.add("Rochu");
+		model.addAttribute("List", list);
+		System.out.println("You are uneder about controller");
+		return "about";
+	}
+	
+	@RequestMapping("/condition")
+	public String conditionalHandler(Model model)
+	{
+		
+		model.addAttribute("IsActive",true);
+		return "condition";
+	}
+	
+	
+	//handling for including fragments
+	@RequestMapping("/service")
+	public String ServiceHandler(Model model)
+	{
+		
+		return "service";
+	}
+}
---
+++ src/test/java/com/practice/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.practice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/about")
+	public String about()
+	{
+		System.out.println("Inside about handler");
+		return "about";
+	}
+}
